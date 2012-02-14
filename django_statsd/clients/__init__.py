93d3afb4ee3a61eddd916e804ae9e2950f2c6cad
Mattias Benjaminsson <mattias@colourandcode.se>
2012-02-14 14:08:03 +0100

    Make it work

--- django_statsd/clients/__init__.py
+++ django_statsd/clients/__init__.py
@@ -9,7 +9,7 @@ _statsd = None
 def get_client():
     client = getattr(settings, 'STATSD_CLIENT', 'statsd.client')
     host = getattr(settings, 'STATSD_HOST', 'localhost')
-    host = socket.gethostbyaddr(host)[2][0]
+    # host = socket.gethostbyaddr(host)[2][0]
     port = getattr(settings, 'STATSD_PORT', 8125)
     prefix = getattr(settings, 'STATSD_PREFIX', None)
     return import_module(client).StatsClient(host, port, prefix)
